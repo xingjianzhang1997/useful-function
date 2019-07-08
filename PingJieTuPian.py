08186b153bf208ff0a1945f9a25af317f56d0508
xingjianzhang1997 <850072282@qq.com>
2019-07-08 21:31:11 +0800

    Create PingJieTuPian.py
    拼接图片函数

---
+++ PingJieTuPian.py
@@ -0,0 +1,35 @@
+# function：可以把文件夹中的所有图片每三个拼接在一起
+# author： xingjianzhang
+# time： 2019/7/8
+
+import os
+from PIL import Image
+from os import listdir
+
+num = 0
+
+# 获取当前文件夹中所有JPG图像
+os.chdir("D:/python/BCI/Data")
+im_list = [Image.open(fn) for fn in listdir() if fn.endswith('.jpg')]
+
+for i in range(1236):     # i=0,i=1
+    # 图片转化为相同的尺寸
+    ims = []
+    for a in im_list[num:num+3]:
+        new_img = a.resize((1280, 1280), Image.BILINEAR)  # bilinear函数使用双线性插值调整images的size
+        ims.append(new_img)
+
+    # 单幅图像尺寸
+    width, height = ims[0].size
+
+    # 创建空白长图
+    result = Image.new(ims[0].mode, (width, height * 3))
+
+    # 拼接图片
+    for x, im in enumerate(ims):
+        result.paste(im, box=(0, x * height))
+
+    # 保存图片
+    result.save('D:/python/BCI/jpg-data/jpg%d.label.jpg' %(i+1))
+    num = num + 3
+
